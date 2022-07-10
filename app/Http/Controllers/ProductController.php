284c2f4325b2282cf33f6b3be049595cdbfec220
kawamotohiroakiTest <kagoshimasyusyoku@gmail.com>
2022-07-11 01:10:28 +0900

    productモデルにメソッド追加

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,6 @@ class ProductController extends Controller
     public function index()
     {
         $bigcategories = DB::table('bigcategory')->get();
-        dump($bigcategories);
         return view('product/index', compact('bigcategories'));
     }
 
@@ -23,28 +22,16 @@ class ProductController extends Controller
 
     public function category($id)
     {
-        $products2 = DB::table('products')->get();
-        $products = DB::table('products') //id被りのため修正
-            ->select('products.id','products.products_name','products.products_code',
-            'products.products_price','products.products_stock','products.products_review','products.products_bigcategory','products.products_image','products.products_size',
-            'products.products_description','products.products_color','products.products_material','products.products_deliverymethod',
-            'products.products_return','products.products_postage','bigcategory.bigcategory_name')
-            ->where('products_bigcategory', $id)
-            ->leftJoin('bigcategory', 'products_bigcategory', '=', 'bigcategory.id')
-            ->get();
-        $count = $products->count();
-        dump($count);
+        $products = \App\Models\Product::getProductCategory($id);
+        $count = \App\Models\Product::getProductCount($products);
         return view('product/category', compact('products', 'count'));
     }
 
     public function search(Request $request)
     {
-        $key = $request->input('key');
-        $query = Product::query();
-        $query->where('products_name', 'LIKE', "%{$key}%");
-        $products = $query->get();
-        $count = $products->count();
-        // dump($products);
+        $key = \App\Models\Product::getProductSearchKey($request);
+        $products = \App\Models\Product::getProductSearch($key);
+        $count = \App\Models\Product::getProductCount($products);
         return view('product/search', compact('key','count','products'));
     }
 
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
 
 class Product extends Model
 {
@@ -28,4 +31,33 @@ class Product extends Model
         'products_deleteflag',
     ];
 
+    // カテゴリ画面の製品情報を取得
+    public static function getProductCategory($id) {
+        $products = DB::table('products') //id被りのため修正
+        ->select('products.id','products.products_name','products.products_code',
+        'products.products_price','products.products_stock','products.products_review','products.products_bigcategory','products.products_image','products.products_size',
+        'products.products_description','products.products_color','products.products_material','products.products_deliverymethod',
+        'products.products_return','products.products_postage','bigcategory.bigcategory_name')
+        ->where('products_bigcategory', $id)
+        ->leftJoin('bigcategory', 'products_bigcategory', '=', 'bigcategory.id')
+        ->get();    
+        return $products;
+    }
+    // 製品の数を取得
+    public static function getProductCount($getProductCategory) {
+        $count = $getProductCategory->count();
+        return $count;
+    }
+    //
+    public static function getProductSearchKey(Request $request) {
+        $key = $request->input('key');
+        return $key;
+    }
+    // 検索画面の製品情報を取得
+    public static function getProductSearch($key) {
+        $query = Product::query();
+        $query->where('products_name', 'LIKE', "%{$key}%");
+        $products = $query->get();
+        return $products;
+    }
 }
