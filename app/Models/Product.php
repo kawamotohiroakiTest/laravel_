5ec5022ef79bcf9d479d5141585bde18b5f96cb8
kawamotohiroakiTest <kagoshimasyusyoku@gmail.com>
2022-07-11 01:32:18 +0900

    modelにコメント追加

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -31,7 +31,12 @@ class Product extends Model
         'products_deleteflag',
     ];
 
-    // カテゴリ画面の製品情報を取得
+    /**
+    * カテゴリページに表示させる製品情報を取得
+    *
+    * @param int $id products_bigcategoryidのID
+    * @return array 製品情報と製品のカテゴリ名
+    */    
     public static function getProductCategory($id) {
         $products = DB::table('products') //id被りのため修正
         ->select('products.id','products.products_name','products.products_code',
@@ -43,17 +48,35 @@ class Product extends Model
         ->get();    
         return $products;
     }
-    // 製品の数を取得
-    public static function getProductCount($getProductCategory) {
-        $count = $getProductCategory->count();
+
+    /**
+    * カテゴリページに表示させる製品情報の数を取得
+    *
+    * @param object $products 製品情報
+    * @return int 製品情報の数
+    */    
+    public static function getProductCount($products) {
+        $count = $products->count();
         return $count;
     }
-    //
+
+    /**
+    * ヘッダーの検索フォームに入力されたkeyを取得
+    *
+    * @param object $request ヘッダーの検索フォームに入力されたkey
+    * @return string $key
+    */    
     public static function getProductSearchKey(Request $request) {
         $key = $request->input('key');
         return $key;
     }
-    // 検索画面の製品情報を取得
+
+    /**
+    * 検索フォームのkeyを元に検索する
+    *
+    * @param string $key 取得したkey
+    * @return object 製品情報
+    */    
     public static function getProductSearch($key) {
         $query = Product::query();
         $query->where('products_name', 'LIKE', "%{$key}%");
