6ede684c157bce583d3234c1a281bdbfcf457096
kawamotohiroakiTest <kagoshimasyusyoku@gmail.com>
2022-07-15 11:27:38 +0900

    cartの商品購入モデル

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -18,14 +18,13 @@ class CartController extends Controller
     //カートの中身を出すメソッドを作る
     public function index(Request $request)
     {
+        //カートの中身を空にしたいとき
         // $request->session()->forget('product_id');
-        // dd($request->session());
         
         $isAddCart = Cart::isAddCart($request);
         if($isAddCart) {
-            $cart_products = Cart::addCart($request);
+            $cart_products = Cart::getCart($request);
         }
-        // dd($cart_products);
         if (!isset($cart_products)) {
             return view('cart/index');
         } else {
@@ -36,70 +35,13 @@ class CartController extends Controller
     //カートに入れた場合はこちら
     public function indexpost(Request $request)
     {
-        if (!$request->session()->has('product_id')) { //カートが空なら配列を作る
-            $request->session()->put('product_id', []);
-        }
-        if ($request->session()->has('product_id')) {
-            $get = $request->session()->get('product_id');
-            if(!in_array($request->product_id, $get)) {
-                $request->session()->push('product_id', $request->input('product_id'));
-            }
-        }
-        $cart_products = [];
-        foreach($get as $key => $value) {
-            $product = Product::where('id', '=', $value)->first();
-            $cart_products[] = $product;
-        }
-        $session = session('product_id');
-        if (!isset($cart_products)) {
-            // return view('cart/index', compact('session'));
-            return redirect()->route('product.index');
-        } else {
-            // return view('cart/index', compact('session'));
-            return redirect()->route('product.index');
-        }
+        Cart::addCart($request);
+        return redirect()->route('product.index');
     }
 
+    //購入
     public function purchase(Request $request) {
-        $product_id_arr = $request->collect("product_id");
-        $wherestr = $product_id_arr->implode(" OR id= ");
-        $products = DB::select('select * from products where id = '.$wherestr);
-        
-        $user = Auth::user();
-
-        foreach($products as $product){
-            $post = new Purchaselog;
-
-            $post->purchase_userid = $user->id;
-            $post->purchase_username = $user->name;
-            $post->purchase_mailaddress = $user->email;
-            $post->purchase_address = $user->address;
-            
-            $product_stock = Product::find($product->id);
-            $product_stock->id = $product_stock->id;
-            $product_stock->products_stock = $product->products_stock-1;
-            $product_stock->save();
-
-            $post->purchase_productid = $product->id;
-            $post->purchase_name = $product->products_name;
-            $post->purchase_code = $product->products_code;
-            $post->purchase_num = 1;
-            $post->purchase_price = $product->products_price;
-            $post->purchase_review = $product->products_review;
-            $post->purchase_tag = $product->products_tag;
-            $post->purchase_bigcategory = $product->products_bigcategory;
-            $post->purchase_size = $product->products_size;
-            $post->purchase_description = $product->products_description;
-            $post->purchase_color = $product->products_color;
-            $post->purchase_material = $product->products_material;
-            $post->purchase_deliverymethod = $product->products_deliverymethod;
-            $post->save();
-            MailController::MailNotification($user->name, $product->products_name, $user->email);
-        };
-
-        //完了メール送信
-        $request->session()->forget('product_id');
-
+        Cart::storeCart($request);
         return view('cart/complete');
     }
     
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -10,9 +10,10 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use App\Models\Product;
-
-
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use App\Models\Mail;
+use App\Http\Controllers\MailController;
 
 
 class Cart extends Authenticatable
@@ -43,12 +44,12 @@ class Cart extends Authenticatable
 
 
     /**
-    * カート商品を追加する
+    * カート商品を出力する
     *
     * @param Request  $request
     * @return array
     */    
-    public static function addCart(Request $request) {
+    public static function getCart(Request $request) {
         $get = $request->session()->get('product_id');
         
         $cart_products = [];
@@ -59,6 +60,79 @@ class Cart extends Authenticatable
         $session = session('product_id');
         return $cart_products;
     }
+
+    /**
+    * カート商品を追加する
+    *
+    * @param Request  $request
+    * @return array
+    */
+    public static function addCart(Request $request) {
+        if (!$request->session()->has('product_id')) { //カートが空なら配列を作る
+            $request->session()->put('product_id', []);
+        }
+        if ($request->session()->has('product_id')) {
+            $get = $request->session()->get('product_id');
+            if(!in_array($request->product_id, $get)) {
+                $request->session()->push('product_id', $request->input('product_id'));
+            }
+        }
+        $cart_products = [];
+        foreach($get as $key => $value) {
+            $product = Product::where('id', '=', $value)->first();
+            $cart_products[] = $product;
+        }
+        $session = session('product_id');
+        return $cart_products;
+    }
+
+    /**
+    * カートの中の商品を購入する
+    *
+    * @param Request  $request
+    * @return 
+    */
+    public static function storeCart(Request $request) {
+        $product_id_arr = $request->collect("product_id");
+        $wherestr = $product_id_arr->implode(" OR id= ");
+        $products = DB::select('select * from products where id = '.$wherestr);
+        
+        $user = Auth::user();
+
+        foreach($products as $product){
+            $post = new Purchaselog;
+
+            $post->purchase_userid = $user->id;
+            $post->purchase_username = $user->name;
+            $post->purchase_mailaddress = $user->email;
+            $post->purchase_address = $user->address;
+            
+            $product_stock = Product::find($product->id);
+            $product_stock->id = $product_stock->id;
+            $product_stock->products_stock = $product->products_stock-1;
+            $product_stock->save();
+
+            $post->purchase_productid = $product->id;
+            $post->purchase_name = $product->products_name;
+            $post->purchase_code = $product->products_code;
+            $post->purchase_num = 1;
+            $post->purchase_price = $product->products_price;
+            $post->purchase_review = $product->products_review;
+            $post->purchase_tag = $product->products_tag;
+            $post->purchase_bigcategory = $product->products_bigcategory;
+            $post->purchase_size = $product->products_size;
+            $post->purchase_description = $product->products_description;
+            $post->purchase_color = $product->products_color;
+            $post->purchase_material = $product->products_material;
+            $post->purchase_deliverymethod = $product->products_deliverymethod;
+            $post->save();
+            //完了メール送信
+            MailController::MailNotification($user->name, $product->products_name, $user->email);
+        };
+        $request->session()->forget('product_id');
+    }
+
+
 
 
 }
